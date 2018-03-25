6489b51b1def62c436dbe9a62cd95a3c6d506fe8
sam-karis <samkaris75@gmail.com>
2018-03-25 21:50:32 +0300

    Test if models are working as required.

---
+++ tests/test_models.py
@@ -0,0 +1,40 @@
+"""Unit test for different models."""
+import unittest
+
+# local imports
+from app.models import users, admin, books, bookHistory
+
+
+class TestModels(unittest.TestCase):
+    """Test if all models are working."""
+
+    def test_user_model(self):
+        """Test number of users."""
+        Users = list()
+        Users.append(users(1, 'Joan', 'joan@andela.com', 'secretpassword'))
+        self.assertEqual(len(Users), 1)
+
+    def test_admin_model(self):
+        """Test number of admin."""
+        Admin = list()
+        Admin.append(admin(1, 'Sam', 'sam@andela.com', 'hardtoguess'))
+        Admin.append(admin(2, 'Sam', 'sam@andela.com', 'hardtoguess'))
+        self.assertEqual(len(Admin), 2)
+
+    def test_books_model(self):
+        """Test number of books."""
+        Books = list()
+        Books.append(books('Data Science', 'Rpeng', '5th',
+                           'Intro to data science with python', '2001', 5))
+        self.assertEqual(len(Books), 1)
+
+    def test_bookHistory_model(self):
+        """Test number of bookHistory ever borrowed."""
+        BookHistory = list()
+        BookHistory.append(bookHistory('Data Science', '1',
+                                       '12-3-2013', '12-4-2013', 'Returned'))
+        self.assertEqual(len(BookHistory), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
