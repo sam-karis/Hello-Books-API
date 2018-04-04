d5242c14569c7c02fb44ed00238ef37bd128233e
sam-karis <samkaris75@gmail.com>
2018-04-04 13:04:20 +0300

    Setup db for challenge three

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,4 @@
 """Random string used to generate hashes that secure things in an app."""
 SECRET = "qwerty098765mnbvcxzmnbvcxz"
 SESSION_TYPE = "qwerty098765mnbvcxzmnbvcxz"
+SQLALCHEMY_DATABASE_URL = "postgresql://localhost/hello_books"
