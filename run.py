ffb2bb7c40a53ed11f4cf1ffb6a7f929ada27923
sam-karis <samkaris75@gmail.com>
2018-04-05 23:55:47 +0300

    Restructure app entry point incorporate database

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-"""installed imports."""
-import os
-
-from app import create_app
-config_name = os.getenv('FLASK_CONFIG')  # config_name = 'development'
-app = create_app(config_name)
+from app import app
 
 
 if __name__ == '__main__':
