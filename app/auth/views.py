3295f2108e4871fde4df74da1afdc0bf589f47ff
sam-karis <samkaris75@gmail.com>
2018-05-30 01:07:14 +0300

    jsonify responses.

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,16 +20,16 @@ def register_user():
     is_admin = request.json.get('is_admin')
 
     res = None
-    if not name or name.strip() == "":
-        res = {'Message': 'Fill in  your name to register'}
+    if not password or len(password.strip()) < 6:
+        res = {'Message': 'Fill in  a valid password to register'}
     if not email or re.match("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
                              email.strip()) is None:
         res = {'Message': 'Fill in a valid email to register'}
-    if not password or len(password.strip()) < 6:
-        res = {'Message': 'Fill in  a valid password to register'}
+    if not name or name.strip() == "":
+        res = {'Message': 'Fill in  your name to register'}
 
     if res is not None:
-        return res
+        return jsonify(res)
     # Check the user with that email exist in the db.
     if not User.get_user_by_email(email):
         new_user = User(name=name, email=email.strip())
